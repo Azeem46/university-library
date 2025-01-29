0664258c8e623a8f55dc0c05ab93a3274615944b
Azeem46 <shaikhazeem4646@gmail.com>
2025-01-29 23:03:13 +0530

    implemented sign-up auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,11 +18,13 @@ import { Input } from "@/components/ui/input"
 import Link from 'next/link';
 import { FIELD_NAMES, FIELD_TYPES } from '@/constants';
 import ImageUpload from './ImageUpload';
+import { toast } from '@/hooks/use-toast';
+import { useRouter } from 'next/navigation';
 
 interface Props <T extends FieldValues>{
   schema: ZodType<T>,
   defaultValues: T,
-  onSubmit: (data: T) => Promise<{success: boolean, error?: string}>
+  onSubmit: (data: T) => Promise<{success?: boolean, error?: string}>
   type: "SIGN_IN" | "SIGN_UP"
 }
 
@@ -32,6 +34,7 @@ const AuthForm = <T extends FieldValues> ({
     defaultValues,
     onSubmit,
 }: Props<T>) => {
+  const router = useRouter();
   const isSignIn = type === "SIGN_IN";
 
   // 1. Define your form.
@@ -41,7 +44,21 @@ const AuthForm = <T extends FieldValues> ({
   })
  
   const handleSubmit: SubmitHandler<T> = async(data) => {
-
+    const result = await onSubmit(data);
+    if(result.success) {
+      toast({
+        title: "Success",
+        description: isSignIn ? "You have successfully signed in."
+        : "You have successfully signed up.",
+      });
+      router.push("/");
+    }else {
+      toast({
+        title: `Error ${isSignIn ? "signing in" : "signing up"}`,
+        description: result.error ?? "An error occurred.",
+        variant: "destructive",
+      })
+    }
   }
 
   return (
