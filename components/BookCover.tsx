7442087f330da76c1066ab68196b199cbc7a7afe
Azeem46 <shaikhazeem4646@gmail.com>
2025-02-03 11:47:47 +0530

    displayed book created from database and updated props for book

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,47 +1,58 @@
-import { cn } from '@/lib/utils';
-import Image from 'next/image';
-import React from 'react'
-import BookCoverSvg from './BookCoverSvg';
+"use client";
+
+import { cn } from "@/lib/utils";
+import React from "react";
+import BookCoverSvg from "@/components/BookCoverSvg";
+import { IKImage } from "imagekitio-next";
+import config from "@/lib/config";
 
 type BookCoverVaraint = "extraSmall" | "small" | "medium" | "regular" | "wide";
 
 const varaintStyles: Record<BookCoverVaraint, string> = {
-    extraSmall: "book-cover_extra_small",
-    small: "book-cover_small",
-    medium: "book-cover_medium",
-    regular: "book-cover_regular",
-    wide: "book-cover_wide",
-}
+  extraSmall: "book-cover_extra_small",
+  small: "book-cover_small",
+  medium: "book-cover_medium",
+  regular: "book-cover_regular",
+  wide: "book-cover_wide",
+};
 interface Props {
-    className?: string;
-    varaint?: BookCoverVaraint;
-    coverColor?: string;
-    coverImage?: string;
+  className?: string;
+  varaint?: BookCoverVaraint;
+  coverColor?: string;
+  coverImage?: string;
 }
 
 const BookCover = ({
-    className,
-    varaint = "regular",
-    coverColor = "#012B48",
-    coverImage= 'https://placehold.co/400x600.png',
+  className,
+  varaint = "regular",
+  coverColor = "#012B48",
+  coverImage = "https://placehold.co/400x600.png",
 }: Props) => {
   return (
-    <div className={cn("relative trasition-all duration-300", varaintStyles[varaint], className)}>
-    <BookCoverSvg 
-    coverColor={coverColor}
-    />
-    <div className="absolute z-10"
-    style={{left: "12%", width: "87.5%", height: "88%"}}
+    <div
+      className={cn(
+        "relative trasition-all duration-300",
+        varaintStyles[varaint],
+        className
+      )}
     >
-        <Image 
-        src={coverImage} 
-        alt="Book cover"
-        fill
-        className='rounded-sm object-fill'
+      <BookCoverSvg coverColor={coverColor} />
+      <div
+        className="absolute z-10"
+        style={{ left: "12%", width: "87.5%", height: "88%" }}
+      >
+        <IKImage
+          path={coverImage}
+          urlEndpoint={config.env.imagekit.urlEndpoint}
+          alt="Book cover"
+          fill
+          className="rounded-sm object-fill"
+          loading="lazy"
+          lqip={{ active: true }}
         />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default BookCover
+export default BookCover;
